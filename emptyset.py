1bc588787b61b43e2c46541aeca42bde84448b2f
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-11-13 19:24:03 +0530

    Add files via upload

---
+++ emptyset.py
@@ -0,0 +1,14 @@
+a={1,2,3,4,5,1} # 1 is  Not Printed bcs set is non repetitive 
+
+print(type(a))
+print(a)
+
+
+# Empty Sets Correct Format:-
+c =set()
+print(c)
+
+# Empty Set Wrong Format:-
+# a={}
+# print(a)
+# This Will Create A Empty dictionary
